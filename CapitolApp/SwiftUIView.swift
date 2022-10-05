5b8eda29caabaac08da4647960502f726942d076
Bret Leupen <bret@atomicrobot.com>
2022-10-05 07:50:48 -0400

    Added UI Tests for performance metrics

--- CapitolApp/SwiftUIView.swift
+++ CapitolApp/SwiftUIView.swift
@@ -45,6 +45,7 @@ struct SwiftUIView: View {
                             }
                         }
                     }
+                    .accessibilityIdentifier(state.abbreviation)
                 }
             }
         }
