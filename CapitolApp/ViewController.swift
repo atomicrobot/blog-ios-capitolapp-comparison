643830f3044e0e10697e995a5af03bd4c057182f
Bret Leupen <bret@atomicrobot.com>
2022-10-05 15:17:37 -0400

    updated combine chain to tryMap

--- CapitolApp/ViewController.swift
+++ CapitolApp/ViewController.swift
@@ -66,7 +66,7 @@ class ViewController: UITableViewController {
     func loadUSStates() -> AnyPublisher<StateModel, Error> {
         return URLSession.shared.dataTaskPublisher(for: self.jsonURL)
             .map { $0.data }
-            .map { try! self.decoder.decode(StateModel.self, from: $0) }
+            .tryMap { try self.decoder.decode(StateModel.self, from: $0) }
             .mapError { return $0 }
             .eraseToAnyPublisher()
     }
