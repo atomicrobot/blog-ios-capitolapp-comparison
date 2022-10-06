d3a68a2adf8d67234dd23b0f23b6e629ad3ba185
Bret Leupen <bret@atomicrobot.com>
2022-10-06 08:09:58 -0400

    cleaned up swiftUI view

--- CapitolApp/SwiftUIView.swift
+++ CapitolApp/SwiftUIView.swift
@@ -17,23 +17,27 @@ struct SwiftUIView: View {
 
     var body: some View {
         NavigationView {
+            // Create a list will all the states
             List {
+                // Loop thru all the states
                 ForEach(viewModel.capitalData.data, id: \.name) { state in
-                    
+
+                    // Create the region to display for each state using the user location and capital location
                     let region = MKCoordinateRegion.zoom(initialRegion: MKCoordinateRegion(coordinates: [CLLocationCoordinate2D(latitude: Double(state.lat)!, longitude: Double(state.long)!), viewModel.userLocation.coordinate])!)
 
+                    // Each Row is a navigation link to a new view
                     NavigationLink {
                         MapView(region: region,
-                                cityAnnotation: idLocation(name: state.capital,
-                                                           latitude: Double(state.lat)!,
-                                                           longitude: Double(state.long)!)).navigationTitle(state.abbreviation)
+                                currentState: state ).navigationTitle(state.abbreviation)
                     } label: {
+                        // Create the Label for each row in the list
                         VStack {
                             HStack {
                                 Text(state.name)
                                 Spacer()
                             }
                             HStack {
+                                // Calculate and display the User's current location in reference to the capital
                                 let capitalLocation : CLLocation = CLLocation(latitude: Double(state.lat)!, longitude: Double(state.long)!)
                                 let distance = Int(viewModel.userLocation.distance(from: capitalLocation) / 1000)
                                 Text("\(state.capital) \(String(distance)) km away ").font(.footnote)
@@ -49,9 +53,13 @@ struct SwiftUIView: View {
 
 struct MapView: View {
     @State var region: MKCoordinateRegion
-    let cityAnnotation: idLocation
+    let currentState: USState
+
 
     var body: some View {
+        let cityAnnotation: idLocation = idLocation(name: currentState.capital,
+                                                    latitude: Double(currentState.lat)!,
+                                                    longitude: Double(currentState.long)!)
 
         Map(coordinateRegion: self.$region, showsUserLocation: true, annotationItems: [cityAnnotation])
         { item in
